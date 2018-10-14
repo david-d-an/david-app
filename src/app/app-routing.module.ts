e6f2a2721c1b30a056e32e9b9eb2702104f5d5c6
David An <david.d.an@MacBookPro2012.fios-router.home>
2018-10-14 05:55:48 -0400

    Little clean up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HeroesComponent } from './heroes/heroes.component';
 import { PizzaComponent } from './pizza/pizza.component';
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,40 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import { PizzaModule } from './pizza/pizza.module';
 import { HttpTestModule } from './http-test/http-test.module';
-// import { HeroesModule } from './heroes/heroes.module';
-// import { MyCustomModule } from './my-custom/my-custom.module';
 
 import { AppComponent } from './app.component';
 import { MyCustomComponent } from './my-custom/my-custom.component';
 import { UiModule } from './ui/ui.module';
+import { AppRoutingModule } from './app-routing.module';
+
+import { PizzaModule } from './pizza/pizza.module';
+import { HeroesModule } from './heroes/heroes.module';
+// import { MyCustomModule } from './my-custom/my-custom.module';
+
 // import { ListingPageComponent } from './listing-page/listing-page.component';
 // import { HeroesComponent } from './heroes/heroes.component';
 // import { HeroListComponent } from './heroes/hero-list.component';
 
+
 @NgModule({
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    HttpTestModule,
-    PizzaModule,
+    AppRoutingModule,
     UiModule,
+    HeroesModule,
+    PizzaModule,
+    // HttpTestModule,
     // SvctestModule
-    // HeroesModule,
     // MyCustomModule,
-  ],
-  declarations: [
-    AppComponent,
-    MyCustomComponent,
-    // ListingPageComponent,
-    // HeroListComponent,
-    // HeroesComponent
   ],
   providers: [],
   bootstrap: [
-    AppComponent,
-    // MyCustomComponent
+    AppComponent
   ]
 })
 export class AppModule { }
