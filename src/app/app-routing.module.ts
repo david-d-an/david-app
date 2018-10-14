b214aac4bc25fd1ae592fccbd76c3cd63e88fd2f
David An <david.d.an@MacBookPro2012.fios-router.home>
2018-10-14 05:54:55 -0400

    Added page navigation

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { PizzaComponent } from './pizza/pizza.component';
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'pizza', component: PizzaComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule {
+}
