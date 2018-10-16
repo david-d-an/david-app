9a0ffc8b29eadec5a97306f29d24cc69545d2294
David An <david.d.an@MacBookPro2012.local>
2018-10-16 07:51:37 -0400

    Removed jquery and bootstrap from Angular.json

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule.forRoot()
   ],
   exports: [
     LayoutComponent
