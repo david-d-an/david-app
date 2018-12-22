7cc678e818035d392128dbae596925d6d36a5c10
David An <dong82@gmail.com>
2018-12-21 20:55:52 -0500

    Changed name to Angular Pizza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'david-app';
+  title = 'angular-pizza';
   textcolor: string;
 
   changeColor() {
